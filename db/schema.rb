a78568032d99c4bc3ebf05dc8e60ca2cf744f0b3
Joel <joeldmikk@gmail.com>
2014-01-16 14:37:48 -0500

    devise working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140114202659) do
+ActiveRecord::Schema.define(version: 20140114203553) do
 
   create_table "counters", force: true do |t|
     t.string   "unit"
@@ -37,5 +37,10 @@ ActiveRecord::Schema.define(version: 20140114202659) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "workouts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
