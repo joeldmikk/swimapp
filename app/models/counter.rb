1033899bc83e15816c90019bf3c5ce5eb1a14090
Joel <joeldmikk@gmail.com>
2014-01-16 15:37:51 -0500

    working on counter model

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -1,10 +1,28 @@
 class Counter < ActiveRecord::Base
   
   belongs_to :user
+  attr_accessor @miles
+  attr_accessor @workouts
+  attr_accessor @goal
 
-  def intialize(unit)
-    @distance = 0
-    @unit = unit
+  @workouts = Array.new
+
+#Creates a new Counter for the user.
+  def intialize(goal, distance)
+    if goal.nil?
+      @goal = 100
+    else
+      @goal = goal
+    end
+    if distance.nil?
+      @miles = 0
+    else
+      @miles = distance
+    end
+  end
+
+  def total(workouts)
+    @workouts.each { |distance| @miles += distance }
   end
 
 end
