112819e404273e4b61f457bdb1bbe236e716f99f
Joel <joeldmikk@gmail.com>
2014-01-23 16:03:32 -0500

    working on totaller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,5 @@
 class WorkoutsController < ApplicationController
+  before_filter :authenticate_user!
   before_action :set_workout, only: [:show, :edit, :update, :destroy]
 
   # GET /workouts
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -6,6 +6,16 @@ class Workout < ActiveRecord::Base
     MilesConverter.new(units).convert(distance)
   end
 
+  
+
+class Totaler
+  def get_total(user)
+   total = 0
+    user.workouts.each { |x| @@total += x.distance_as_miles }
+    return total
+  end
+end
+
 end
 
 class MilesConverter
